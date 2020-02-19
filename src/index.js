ee17a1d91db3868763e3c140f5d2fc5661644530
thalesleite <thalesaleite@gmail.com>
2020-02-19 12:41:04 +0000

    deploying github pages

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import uuidv4 from 'uuid/v4';
+import validator from 'validator';
+import Hangman from './hangman';
+import getPuzzle from './requests';
+
+console.log(uuidv4());
+
+console.log(validator.isEmail('test@test.com'));
+
+// HTTP (Hypertext Transfer Protocol)
+// Request - What do we want to do
+// Response - What was actually done
+const puzzleEl = document.querySelector('#puzzle');
+const guessesEl = document.querySelector('#guesses');
+let game1;
+
+window.addEventListener('keypress', (e) => {
+  const guess = String.fromCharCode(e.charCode);
+  game1.makeGuess(guess);
+  render();
+});
+
+const render = () => {
+  puzzleEl.innerHTML = '';
+  guessesEl.textContent = game1.statusMessage;
+
+  game1.puzzle.split('').forEach((letter) => {
+    const letterEl = document.createElement('span');
+    letterEl.textContent = letter;
+    puzzleEl.appendChild(letterEl);
+  });
+}
+
+const startGame = async () => {
+  const puzzle = await getPuzzle('2');
+  game1 = new Hangman(puzzle, 5);
+  render();
+}
+
+document.querySelector('#reset').addEventListener('click', startGame);
+
+startGame();
---
+++ src/requests.js
@@ -0,0 +1,52 @@
+const getPuzzle = async (countWord) => {
+  const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${countWord}`);
+
+  if(response.status === 200) {
+    const data = await response.json();
+    return data.puzzle;
+  } else {
+    throw new Error('Unable to get puzzle');
+  }
+}
+
+// const getPuzzle = (countWord) => {
+//   return fetch(`http://puzzle.mead.io/puzzle?wordCount=${countWord}`).then((response) => {
+//     if(response.status === 200) {
+//       return response.json();
+//     } else {
+//       throw new Error('Unable to fetch puzzle');
+//     }
+//   }).then((data) => {
+//     return data.puzzle;
+//   });
+// }
+
+const getCurrentCountry = async () => {
+  const location = await getLocation();
+  
+  return  getCountry(location.country);
+}
+
+const getCountry = async (countryCode) => {
+  const response = await fetch('http://restcountries.eu/rest/v2/all');
+
+    if(response.status === 200) {
+      const data = await response.json();
+
+      return data.find((country) => country.alpha2Code === countryCode);
+    } else {
+      throw new Error('Unable to fetch the country');
+    }
+}
+
+const getLocation = async () => {
+  const response = await fetch('https://ipinfo.io/json?token=29074c75cebdcc');
+
+    if(response.status === 200) {
+      return response.json();
+    } else {
+      throw new Error('Unable to get the current location');
+    }
+}
+
+export { getPuzzle as default }
